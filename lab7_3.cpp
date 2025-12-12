ba84dce5428b35151eac9ee6c4b01a9c25b624fa
PhanupongWOwwww <Zozackgt@gmail.com>
2025-12-12 23:21:22 +0700

    lab7_3.cpp completed

--- lab7_3.cpp
+++ lab7_3.cpp
@@ -2,11 +2,12 @@
 using namespace std;
 
 char before(char x){
-	//Write your function definition here
+    if(x < 'A' || x > 'Z') return '0';
+    if(x == 'A') return 'Z';
+    return x - 1;
 }
 
 int main(){
-	//Test Case
 	cout << before('A') << "\n";
 	cout << before('B') << "\n";
 	cout << before('P') << "\n";
